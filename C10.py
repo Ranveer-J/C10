ebbd0f4306b9ea7dc4e602b3d9cd57029cdae2c2
Ranveer-J <83578974+Ranveer-J@users.noreply.github.com>
2024-12-16 18:24:26 +0000

    Add files via upload

---
+++ C10.py
@@ -0,0 +1,14 @@
+class employee:
+
+    def __init__(self):
+        print("Employee created")
+
+    def __del__(self):
+            print("Employee deleted")
+
+
+obj = employee()
+del employee
+
+
+
